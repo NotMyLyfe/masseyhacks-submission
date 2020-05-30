0c4108f126e1809f228d4f875c702d4b8a521fbf
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-05-30 15:01:41 -0400

    prototype for data command

--- bot/covid.py
+++ bot/covid.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import requests
 import logging  # remove logging in release
 
 logger = logging.getLogger('discord')
@@ -9,6 +10,7 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix='c!', description="A bot with COVID-19 Statistics")
+bot.remove_command('help')
 
 file = 'token.txt'
 with open(file, 'r') as token:
@@ -18,6 +20,21 @@ with open(file, 'r') as token:
 @bot.event
 async def on_ready():
     print('The bot has logged in under the account ' + bot.user.name + "#" + bot.user.discriminator)
+
+
+@bot.command()
+async def help(ctx):
+    emb = discord.Embed(title="COVID Data Commands", description="The commands in this bot", color=0xff0000)
+    emb.add_field(name="c!help", value="Displays this help embed.", inline=False)
+    emb.add_field(name="c!data [country code]", value="Displays the statistic for the specified country.", inline=False)
+    await ctx.send(embed=emb)
+
+
+@bot.command()
+async def data(ctx, *args):
+    dater = "https://who.com/api-idk/{}data"
+    dater = dater.format(args[0].lower)
+    await ctx.send(requests.request(dater))
 
 
 while True:
