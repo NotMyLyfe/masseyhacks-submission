3a71a5241967fe9be5420d6c62ad1e6c99db306e
NotMyLyfe <30644833+NotMyLyfe@users.noreply.github.com>
2020-05-31 12:31:40 -0400

    Fixed issues

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ Session(app)
 def index():
     worldInfo = requests.get("https://api.covid19api.com/world/total").json()
     return render_template('index.html', recovered=worldInfo.get('TotalRecovered'), deaths=worldInfo.get('TotalDeaths'), confirmed=worldInfo.get('TotalConfirmed'))
-@app.route("about")
+@app.route("/about")
 def about():
     return render_template('about.html')
 
